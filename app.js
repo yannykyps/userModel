1a664946de508c8760c4596c966a3b0259ec1eb6
YannyKyprianou <yannykyps@hotmail.com>
2020-06-12 13:04:24 +0100

    get routes established

---
+++ app.js
@@ -0,0 +1,38 @@
+//require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+require("./models/userModel");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userModelDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
---
+++ models/userModel.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String
+});
+
+const User = new mongoose.model("User", userSchema);
